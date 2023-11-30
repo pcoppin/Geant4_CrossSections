e8c05397ebb4a783f3481a9df586e3766eb91f56
pcoppin <coppinpaul@hotmail.com>
2023-11-30 14:21:03 +0100

    Add missing files

---
+++ Hadr00/ReadPlotCrossSections/ProcessG4OutFile.py
@@ -0,0 +1,35 @@
+from __future__ import print_function, division, absolute_import
+from glob import glob
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+import os, sys
+
+Primary = sys.argv[1]
+Material = sys.argv[2]
+Model = "FTFP"
+
+in_file = "../build/hadr00.out"
+with open(in_file) as f:
+    lines = f.readlines()
+    
+start_where = ["### Fill Cross Sections for" in x for x in lines]
+start_index = np.where(start_where)[0][0] + 4
+if( lines[start_index].split()[0]!="0" ):
+    raise Exception("Index first data point should be zero")
+
+variable_names = lines[start_index-2].split()
+data = {name: [] for name in variable_names}
+for line in lines[start_index:]:
+    if( line[0]=="-" ):
+        break
+    else:
+        data_i = line.split()
+        for name, x in zip(variable_names,data_i):
+            data[name].append( float(x) )
+
+with open("../ReadPlotCrossSections/CrossSections_10_03/Geant4_{}_{}_on_{}.txt".format(Model,Primary,Material), "w") as f:
+    f.write("# Energy (GeV)      Cross section (barn)\n")
+    for E, s in zip(data['E(MeV)'],data['Inelastic(b)']):
+        f.write("  {:<17.3e} {:.3e}\n".format(1e-3*E,s))
---
+++ setup_cvmfs_with_Geant4-10.3.2.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+wd=$(pwd)
+export cvbase=/cvmfs/dampe.cern.ch/centos7
+export PYTHON_VERSION=2.7.14
+export CMAKE_VERSION=3.9.3
+export BOOST_VERSION=1.65.1
+export BOOST_VERSION_STR=1_65_1
+export SCONS_VERSION=2.5.1
+export XERCESC_VERSION=3.2.0
+export ROOT_VERSION=5-34-36
+export GEANT4_VERSION=4.10.03.p02
+export GEANT4_VERSION_STR=4_10_03_p02
+# export GEANT4_VERSION=4.10.05.p01
+# export GEANT4_VERSION_STR=4_10_05_p01
+export RELEASE_DIR=${cvbase}/opt/DMPSW
+export LD_LIBRARY_PATH=${cvbase}/lib:${cvbase}/lib64
+export CC=${cvbase}/opt/rh/devtoolset-4/root/usr/bin/gcc
+export CXX=${cvbase}/opt/rh/devtoolset-4/root/usr/bin/g++
+export FC=${cvbase}/opt/rh/devtoolset-4/root/usr/bin/gfortran
+# use conda's python
+export PATH=${cvbase}/opt/conda/bin:$PATH
+export LD_LIBRARY_PATH=${cvbase}/opt/conda/lib:${LD_LIBRARY_PATH}
+# add cvmfs-bin path
+export PATH=${cvbase}/bin:${PATH}
+# cleanup any existing LD_LIBRARY_PATH stuff
+#export LD_LIBRARY_PATH=/cvmfs/dampe.cern.ch/centos7/opt/conda/lib/:/cvmfs/dampe.cern.ch/centos7/lib:/cvmfs/dampe.cern.ch/centos7/lib64:/cvmfs/dampe.cern.ch/centos7/opt/rh/devtoolset-4/root/usr/lib:/cvmfs/dampe.cern.ch/centos7/opt/rh/devtoolset-4/root/usr/lib64
+# set up the newer GCC from the devtoolset
+source ${cvbase}/opt/rh/devtoolset-4/enable
+# adding root
+cd ${cvbase}/opt/root-5*
+source ./bin/thisroot.sh
+cd ${wd}
+
+# adding mysql connector
+cd ${cvbase}/opt/mysql-*
+export SQL_PATH=`pwd`
+export LIBRARY_PATH=${SQL_PATH}/lib:${LIBRARY_PATH}
+export LD_LIBRARY_PATH=${SQL_PATH}/lib:${LD_LIBRARY_PATH}
+export DYLD_LIBRARY_PATH=${SQL_PATH}/lib:${DYLD_LIBRARY_PATH}
+export CPATH=${SQL_PATH}/include:${CPATH}
+cd ${wd}
+
+# adding boost
+export LIBRARY_PATH=${cvbase}/lib:${cvbase}/lib64:${cvbase}/opt/conda/lib:$LIBRARY_PATH
+
+# adding HEPMC 2018-10-19
+export HEPMC_PREFIX=${cvbase}/opt/HepMC-2.06.09
+
+# adding CRMC 2018-10-20
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${cvbase}/opt/G4CRMC/lib
+export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:${cvbase}/opt/G4CRMC/lib
+export CPATH=${CPATH}:${cvbase}/opt/G4CRMC/src
+export CPATH=${CPATH}:${cvbase}/src/crmc/src
+export CRMC_CONFIG_FILE=${cvbase}/opt/G4CRMC/crmc.param
+
+# adding Geant4
+cd ${cvbase}/opt/geant4.10.03p02/share/Geant4-10.3.2/geant4make
+source ./geant4make.sh
+cd ${wd}
+
+rootenv6(){
+  unset ROOTSYS
+  wd=$(pwd)
+  cd ${cvbase}/opt/root-6*/bin
+  source ./thisroot.sh
+  cd ${wd}
+}
+
+dampe_init(){
+  if (( $# == 0 )); then release="latest";
+  else release=$1; fi
+  workdir=$(pwd)
+  echo "DAMPE software setup - core"
+  cd ${RELEASE_DIR}/${release}
+  echo "DmpBuild: $(readlink -f .)"
+  source ./bin/thisdmpsw.sh
+  cd ${workdir}
+}
+
+set_virtualenv(){
+  if (( $# == 0 )); then MY_PATH=${HOME};
+  else MY_PATH=$1; fi
+  export WORKON_HOME=${MY_PATH}/virtualEnvs/
+  mkdir -p $WORKON_HOME
+  source $(which virtualenvwrapper.sh)
+}
